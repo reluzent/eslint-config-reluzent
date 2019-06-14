4a6b503769a5944da066533590251b7b987e09fa
Ezequiel mross <ezequielmross@gmail.com>
2019-06-14 11:02:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    globals: {
+        MyGlobal: true
+    },
+    "root": true,
+    "parserOptions": {
+      "parser": "babel-eslint",
+      "sourceType": "module"
+    },
+    "env": {
+      "browser": true
+    },
+    "globals": {
+      "workbox": true,
+      "importScripts": true
+    },
+    "extends": [
+      "@vue/eslint-config-standard",
+      "plugin:vue/essential",
+      "@vue/standard"
+    ],
+    "rules": {
+      "arrow-parens": 0,
+      "generator-star-spacing": 0,
+
+      "no-unused-vars": 1,
+      "space-before-function-paren": 0,
+      "one-var": 0,
+      "curly": 0,
+      "no-multi-spaces": 0,
+      "key-spacing": 0,
+      "standard/no-callback-literal": 0,
+
+      "object-shorthand": 0,
+      "prefer-arrow-callback": 0,
+      "prefer-const": 2,
+      "quotes": ["error", "single"],
+      "prefer-spread": 2,
+      "require-await": 2,
+      "prefer-rest-params": 1
+    }
+}
